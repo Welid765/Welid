20a57f5de6044c5e960061b1ee9e5aea06549fff
Welid765 <164154868+Welid765@users.noreply.github.com>
2024-11-07 15:58:03 +0000

    fileBinario.c

--- File/fileHanafi/FileBinariHanafi/fileBinarioA.c
+++ File/fileHanafi/FileBinariHanafi/fileBinarioA.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h> 
 #include <string.h>
-#define N 10 // Numero costante di record per il file
+#define N 10
 
 // Definizione della struttura Studente
 typedef struct {
-    char cognome[50]; // Cognome dello studente
-    int voti[3];      // Array di 3 voti per lo studente
+    char cognome[50];
+    int voti[3];      
 } Studente;
 
 // Dichiarazione delle funzioni
@@ -19,25 +19,24 @@ void Stampainformazioni();
 int main() {
     char cogn[60];
 
-    // Scrive i dati di N studenti nel file
+   
     scriviFile();
     
-    // Legge e visualizza i dati degli studenti dal file
     leggiFile();
 
-    // Chiede all'utente un cognome da cercare nel file
+ 
     printf("\nInserisci un cognome da cercare: ");
     scanf("%s", cogn);
     int cont = Ricercacognome(cogn);
     
-    // Mostra il risultato della ricerca
+   
     if (cont > 0) {
         printf("Il cognome %s è presente %d volte\n", cogn, cont);
     } else { 
         printf("Il cognome %s non è presente\n", cogn);
     }
 
-    // Stampa ulteriori informazioni sugli studenti
+    
     Stampainformazioni();
 
     return 0;
@@ -53,12 +52,12 @@ void scriviFile() {
         return;
     }
 
-    // Ciclo per inserire i dati di N studenti
+  
     for (int i = 0; i < N; i++) {
         printf("Inserisci un cognome: ");
         scanf("%s", studente[i].cognome);
         
-        // Ciclo per inserire 3 voti per ogni studente
+      
         for (int j = 0; j < 3; j++) {
             printf("Inserisci il voto: ");
             scanf("%d", &studente[i].voti[j]);
@@ -71,7 +70,6 @@ void scriviFile() {
 }
 
 // Funzione per leggere e visualizzare i dati degli studenti dal file binario
-// Usa il controllo di fine file per la lettura, indipendente da N
 void leggiFile() {
     FILE *file = fopen("studenti1.dat", "rb"); // Apertura del file in modalità lettura binaria
     Studente studente;
@@ -85,7 +83,6 @@ void leggiFile() {
     while (fread(&studente, sizeof(studente), 1, file) == 1) {
         printf("Cognome: %s\n", studente.cognome);
         
-        // Stampa i voti dello studente
         printf("Voti: ");
         for (int i = 0; i < 3; i++) {
             printf("%d ", studente.voti[i]);
@@ -132,7 +129,6 @@ void Stampainformazioni() {
     while (fread(&studente, sizeof(Studente), 1, file) == 1) {
         int Max = 0, Min = 10, somma = 0;
         
-        // Calcola somma, voto massimo e minimo
         for (int i = 0; i < 3; i++) {
             somma += studente.voti[i];
             if (studente.voti[i] > Max) {
@@ -143,7 +139,7 @@ void Stampainformazioni() {
             }
         }
         
-        // Calcola la media
+        
         float media = (float) somma / 3;
         
         // Stampa le informazioni dello studente
