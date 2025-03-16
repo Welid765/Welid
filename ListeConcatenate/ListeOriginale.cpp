73f2876fcfe943f577b4455be9ab1dc17e8bc45a
Welid765 <164154868+Welid765@users.noreply.github.com>
2025-03-16 23:08:20 +0000

    done

--- ListeConcatenate/ListeOriginale.cpp
+++ ListeConcatenate/ListeOriginale.cpp
@@ -2,139 +2,223 @@
 
 using namespace std;
 
-
 typedef struct Nodo
 {
-int valore;
-Nodo *next;
-} Nodo;
 
-Nodo *Inserimentointesta(Nodo*s);
-Nodo *Inserimentoincoda(Nodo*s);
-void StampaLista(Nodo *s);
+int info;
+Nodo *next;
+
+}Nodo;
+
+
+Nodo *Inserimentointesta(Nodo *s);
+Nodo *Inserimentoincoda(Nodo *s);
+void Stampa(Nodo *s);
 Nodo *Cancellazioneintesta(Nodo *s);
-Nodo *Cancellazioneincoda(Nodo*s);
+Nodo *Cancellazioneincoda(Nodo *s);
+Nodo *Ricerca(Nodo *s,int );
+int Conta(Nodo *s);
 
 
 int main()
 {
+  Nodo *s = nullptr;
+  Nodo *d = nullptr;
+  int a;
 
-    Nodo *s = nullptr;
+    cout << "Inserimento in testa : " << endl;
+    s = Inserimentointesta(s);
+    s = Inserimentointesta(s);
+    s = Inserimentointesta(s);
+    Stampa(s);
 
-    cout << "Inserimento in Testa : " << endl;
-    s = Inserimentointesta(s);
-    s = Inserimentointesta(s);
-    s = Inserimentointesta(s);
-    StampaLista(s);
-    
     cout << "Cancellazione in testa : " << endl;
     s = Cancellazioneintesta(s);
-    StampaLista(s);
+    s = Cancellazioneintesta(s);
+    Stampa(s);
 
+    
+   cout <<"Inserimento in coda : " << endl;
+   s = Inserimentoincoda(s);
+   s = Inserimentoincoda(s);
+   s = Inserimentoincoda(s);
+   s = Inserimentoincoda(s);
+   Stampa(s);
+   
+   cout <<"Cancellazione in coda : " << endl;
+   s = Cancellazioneincoda(s);
+   Stampa(s);
+  
+   cout << "Inserisci il valore da cercare : " <<endl;
+   cin >> a;
+   d = Ricerca(s,a);
 
-    cout << "Inseriemnto in Coda : " << endl;
-    s = Inserimentoincoda(s);
-    s = Inserimentoincoda(s);
-    StampaLista(s);
+   if(d != nullptr)  //Ricorda
+   {
 
-    cout << "Cancellazione in coda : " << endl;
-    s = Cancellazioneincoda(s);
-    StampaLista(s);
+    cout << "Elemento trovato : " << a << endl;
+   }
 
-    return 0;
+   else
+   {
+     cout << "Elemento non trovato : " << a << endl;
+   }
+
+   int Cont = Conta(s);
+   cout << "Numero volte dei nodi : " << Cont << endl;
+
+return 0;
+
 }
 
-Nodo *Inserimentointesta(Nodo*s)
+
+Nodo *Inserimentointesta(Nodo *s)
 {
 Nodo *p = new Nodo;
-p -> valore = 3;
-p -> next = s;   
-return p;           
+p -> info = 3;
+p -> next = s;
+
+return p;
 }
 
 
-Nodo *Inserimentoincoda(Nodo*s)
+Nodo *Inserimentoincoda(Nodo *s)
 {
-  Nodo *p = new Nodo;
-  p -> valore = 3;
-  p ->next = nullptr;
+   Nodo *p = new Nodo;
+   p -> info = 4;
+   p -> next = nullptr;
 
-  if(s == nullptr)
-  {
-    return p;
-  }
+   if(s == nullptr)
+   {
+      return p;
+   }
 
-  Nodo *aux = s;
+   Nodo *aux = s;
+
   while(aux -> next != nullptr)
   {
     aux = aux -> next;
   }
-   aux -> next = p;
-   return s;
+
+  aux -> next = p;
+
+  return s;
 }
 
-void StampaLista(Nodo *s)
+void Stampa(Nodo *s)
 {
-   Nodo *aux = s;
-   while(aux != nullptr)
-   {
-      cout << aux->valore << endl;
-      aux = aux -> next;
-   }
+
+  Nodo *aux = s;
+
+  while(aux != nullptr)
+  {
+
+    cout << aux -> info << endl;
+    aux = aux -> next;
+
+  }
 
 }
+
 
 Nodo *Cancellazioneintesta(Nodo *s)
 {
+  Nodo *aux = s;
+
   if(s == nullptr)
   {
     return nullptr;
   }
 
-  if (s->next == nullptr) {  
-    delete s;             
-    return nullptr;  
-  }  
-    Nodo *aux = s;
-   
-     s = s -> next; 
-     delete aux;
-
-     return s;
-    
+  if(s -> next == nullptr)
+  {
+    delete s;
+    return nullptr;
   }
- 
 
+  if(aux -> next != nullptr)
+  {
 
+    aux = aux -> next;
 
-Nodo *Cancellazioneincoda(Nodo*s)
-{
+  }
 
-if(s == nullptr)
-{
-  return nullptr;
-}
-
-if(s -> next == nullptr)
-{
   delete s;
-  return nullptr;
+  return aux;
+
 }
 
-Nodo *aux = s;
-Nodo *prev = nullptr;
-
-while(aux -> next != nullptr)
+Nodo *Cancellazioneincoda(Nodo *s)
 {
-prev = aux;
-aux = aux -> next;
+
+  Nodo *aux = s;
+  Nodo *prev = nullptr;
+
+  if(s  == nullptr)
+  {
+    return nullptr;
+  }
+
+  if(s -> next == nullptr)
+  {
+    delete s;
+    return nullptr;
+  }
+
+  while(aux ->next != nullptr)
+  {
+      prev = aux;
+      aux = aux -> next;
+  }
+
+  delete aux;
+   
+  prev -> next = nullptr;
+
+  return s;
+
 }
 
-delete aux;
-prev -> next = nullptr;
 
-return prev;
+Nodo *Ricerca(Nodo*s, int valore)
+{
+  Nodo *aux =  s;
+
+  if(aux -> info == valore)
+  {
+      return aux;
+  }
+
+  while(aux ->next != nullptr)
+  {
+    aux = aux -> next;
+
+  if(aux ->info == valore) //Ricorda
+  {
+    return aux;
+  }
 }
+return nullptr;
+}
+
+int Conta(Nodo *s)
+{
+  int cont = 0;
+
+  Nodo *aux = s;
+  
+  while(aux != nullptr)
+  {
+    cont++;
+    aux = aux -> next;
+  }
+return cont;
+}
+
+
+
+
+
 
 
 
